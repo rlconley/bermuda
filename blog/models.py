c3e73684541711688711b65be0bf208873ea62e3
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2018-04-05 22:19:42 -0300

    remove photo field

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    link = models.URLField(blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
